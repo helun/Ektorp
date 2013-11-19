f1af5d2500f7dc030ff11bafa9021ccfcbf4f478
Daniel Williams <daniel.williams@gmail.com>
2013-11-19 06:28:13 -0700

    another fix to a misspelling

--- org.ektorp.spring/src/main/java/org/ektorp/spring/HttpClientFactoryBean.java
+++ org.ektorp.spring/src/main/java/org/ektorp/spring/HttpClientFactoryBean.java
@@ -191,7 +191,7 @@ public class HttpClientFactoryBean implements FactoryBean<HttpClient>, Initializ
 		LOG.debug("cleanupIdleConnections: {}", cleanupIdleConnections);
 		LOG.debug("enableSSL: {}", enableSSL);
 		LOG.debug("relaxedSSLSettings: {}", relaxedSSLSettings);
-		LOG.debug("useEpectContinue: {}", useExpectContinue);
+		LOG.debug("useExpectContinue: {}", useExpectContinue);
 
 		
 		client = new StdHttpClient.Builder()
