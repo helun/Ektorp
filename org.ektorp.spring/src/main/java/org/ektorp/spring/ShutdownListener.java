86c9ff78399b9459ee60549245e590db3d65277b
David Venable <dev@venabledomain.com>
2014-05-02 15:03:24 -0500

    Added additional documentation to the ShutdownListener.

--- org.ektorp.spring/src/main/java/org/ektorp/spring/ShutdownListener.java
+++ org.ektorp.spring/src/main/java/org/ektorp/spring/ShutdownListener.java
@@ -6,7 +6,13 @@ import org.springframework.context.event.ContextClosedEvent;
 
 /**
  * Provides proper shutdown for Ektorp within a Spring application context.
-
+ *
+ * Add the following line to your Spring application context.
+ * <pre>
+ * {@code
+ * <bean class="org.ektorp.spring.ShutdownListener"/> }
+ * </pre>
+ *
  * @author David Venable
  */
 public class ShutdownListener implements ApplicationListener<ContextClosedEvent>
