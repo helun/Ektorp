1d430481461f97f547eeef7ad6b8a64fae9fe01f
Rodrigo Witzel <rwitzel75@googlemail.com>
2015-01-09 17:29:43 +0100

    added: method ViewResult.Row.getIdAsNode()   so that it is possible to find out whether or not an ID property exists changed: method ViewResult.Row.getId()   so that the method does not throw a NullPointerException when there is no property 'id'
    This is useful when (A) the view result is grouped or reduced
    and (B) the client code is generic so that it does not know whether
    or not the view result contains an ID property.

--- org.ektorp/src/main/java/org/ektorp/ViewResult.java
+++ org.ektorp/src/main/java/org/ektorp/ViewResult.java
@@ -154,8 +154,12 @@ public class ViewResult implements Iterable<ViewResult.Row>, Serializable {
 		}
 
 		public String getId() {
-			return rowNode.get(ID_FIELD_NAME).textValue();
+			return rowNode.findPath(ID_FIELD_NAME).textValue();
 		}
+
+        public JsonNode getIdAsNode() {
+            return rowNode.findPath(ID_FIELD_NAME);
+        }
 
 		public String getKey() {
 			return nodeAsString(getKeyAsNode());
