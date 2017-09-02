fc4f9dacc29da387a7c64bc344af91c88757aaff
Yann ROBERT <yann.robert@anantaplex.fr>
2017-09-03 00:29:26 +0200

    prepare for Jackson 2.9.0 : Jackson 2.9.0 has some method removed (deprecated since 2.7.0)

--- org.ektorp/src/main/java/org/ektorp/impl/StdObjectMapperFactory.java
+++ org.ektorp/src/main/java/org/ektorp/impl/StdObjectMapperFactory.java
@@ -47,7 +47,12 @@ public class StdObjectMapperFactory implements ObjectMapperFactory {
 
 	private void applyDefaultConfiguration(ObjectMapper om) {
 		om.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, this.writeDatesAsTimestamps);
+
+		// method was removed in jackson 2.9.0
 		om.getSerializationConfig().withSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+		// method introduced in jackson 2.7.0
+		// om.getSerializationConfig().withPropertyInclusion(JsonInclude.Value.empty().withValueInclusion(JsonInclude.Include.NON_NULL));
 	}
 
 }
